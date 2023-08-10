1ecc7c9a230bae59dcfcf370f22cf41779b3342c
kakadiya trupesh rashikabhai <126761188+ktrupesh@users.noreply.github.com>
2023-08-09 23:42:55 -0700

    Add files via upload

---
+++ Assessment/Student-Registaration-form/Advance-Web-Designing/js/min.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("ul");
+
+hamburger.addEventListener('click' , () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("ul").forEach(n => n.
+    addEventListener('click' , () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
+)
